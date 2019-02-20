06c1aa8c457587323359b0d09a514b1924ac14f5
sebastian <sebastian@kallisto.fritz.box>
2019-02-20 12:07:08 +0100

    cleanup

--- dawn-finance/dawn-finance-logic/src/main/java/de/slag/dawn/core/service/PersistService.java
+++
@@ -1,7 +0,0 @@
-package de.slag.dawn.core.service;
-
-public interface PersistService {
-	
-	void save(Object o);
-
-}
--- dawn-finance/dawn-finance-logic/src/main/java/de/slag/dawn/core/service/impl/AbstractPersistService.java
+++
@@ -1,9 +0,0 @@
-package de.slag.dawn.core.service.impl;
-
-public abstract class AbstractPersistService {
-	
-	public void save(Object o) {
-		//save;
-	}
-
-}
